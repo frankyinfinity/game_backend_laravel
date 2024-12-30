89bb152eca74fe2392d66fa482da98ff6661066e
companyinfinity <companyinfinity@gmail.com>
2024-12-30 16:14:53 +0100

    install laravel adminlte 3

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()->create([
+            "name"=> "Admin",
+            "email"=> "admin@email.it",
+            "password"=> bcrypt("admin"),
+        ]);
+    }
+}
