1f03c1d70456b707085b0708141d9908f9bc1231
frankyinfinity <companyinfinity2024@gmail.com>
2025-03-11 12:44:38 +0000

    init BasicDraw

---
+++ app/Custom/BasicDraw.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Custom;
+
+class BasicDraw
+{
+    private $type;
+    private $color;
+
+    public function __construct($type)
+    {
+        $this->type = $type;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function printJson() 
+    {
+        return json_encode([
+            'type' => $this->type,
+            'color' => $this->color
+        ]);
+    }
+
+}
