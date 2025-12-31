d94c6c20d5efb4133cfa3a7e4f207d84252ba362
companyinfinity <companyinfinity@gmail.com>
2025-12-31 12:56:15 +0100

    feat: Enhance TestDocker command to check and build Docker image if not present

--- app/Console/Commands/TestDocker.php
+++ app/Console/Commands/TestDocker.php
@@ -28,8 +28,45 @@ class TestDocker extends Command
     {
 
         putenv('DOCKER_HOST=tcp://127.0.0.1:2375');
-        
         $docker = Docker::create();
+        $imageName = 'entity:latest';
+        
+        // Controlla se l'immagine esiste
+        try {
+            $images = $docker->imageList();
+            $imageExists = collect($images)->contains(fn($img) => 
+                in_array($imageName, $img->getRepoTags() ?? [])
+            );
+            
+            if (!$imageExists) {
+                $this->info("Immagine '$imageName' non trovata. Costruisco l'immagine...");
+                
+                // Leggi il Dockerfile
+                $dockerfilePath = base_path('docker/entity/Dockerfile');
+                if (!file_exists($dockerfilePath)) {
+                    $this->error("Dockerfile non trovato in: $dockerfilePath");
+                    return 1;
+                }
+                
+                // Costruisci l'immagine
+                $docker->imageBuild(
+                    fopen($dockerfilePath, 'r'),
+                    [
+                        'dockerfile' => 'Dockerfile',
+                        't' => $imageName,
+                    ]
+                );
+                
+                $this->info("Immagine '$imageName' costruita con successo!");
+            } else {
+                $this->info("Immagine '$imageName' già presente.");
+            }
+        } catch (\Exception $e) {
+            $this->error("Errore nel controllo/costruzione dell'immagine: " . $e->getMessage());
+            return 1;
+        }
+        
+        // Mostra i container
         $containers = $docker->containerList(['all' => true]);
         
         $this->table(['ID', 'Image', 'Status'], 
