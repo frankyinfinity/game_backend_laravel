287302ce1f8287eecad7cbf3e95242d06b0ba389
companyinfinity <companyinfinity@gmail.com>
2026-01-13 15:26:06 +0100

    improve websocket logic movement

--- resources/js/function/entity/movement_ws.blade.php
+++ resources/js/function/entity/movement_ws.blade.php
@@ -11,10 +11,12 @@
         }
 
         let wsUrl = 'ws://' + window.location.hostname + ':' + port;
-        let ws = new WebSocket(wsUrl);
+        
+        // Global cache
+        window.gameWebSockets = window.gameWebSockets || {};
+        let ws = window.gameWebSockets[port];
 
-        ws.onopen = function() {
-            console.log('WS Connected to ' + wsUrl);
+        const sendCommand = () => {
             ws.send(JSON.stringify({
                 command: 'move',
                 params: {
@@ -23,15 +25,30 @@
             }));
         };
 
-        ws.onmessage = function(event) {
-            let response = JSON.parse(event.data);
-            console.log('WS Response:', response);
-            ws.close();
-        };
+        if (!ws || ws.readyState === WebSocket.CLOSED || ws.readyState === WebSocket.CLOSING) {
+            ws = new WebSocket(wsUrl);
+            window.gameWebSockets[port] = ws;
 
-        ws.onerror = function(error) {
-            console.error('WS Error:', error);
-        };
+            ws.onopen = function() {
+                console.log('WS Connected to ' + wsUrl);
+                sendCommand();
+            };
+
+            ws.onmessage = function(event) {
+                let response = JSON.parse(event.data);
+                console.log('WS Response:', response);
+            };
+
+            ws.onerror = function(error) {
+                console.error('WS Error:', error);
+            };
+        } else {
+             if (ws.readyState === WebSocket.OPEN) {
+                sendCommand();
+            } else if (ws.readyState === WebSocket.CONNECTING) {
+                ws.addEventListener('open', sendCommand, { once: true });
+            }
+        }
 
     }
     window['__name__']();
