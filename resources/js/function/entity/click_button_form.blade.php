1f3aee0bc489387325a38af2fe8743263215ed7c
companyinfinity <companyinfinity@gmail.com>
2026-01-20 16:43:16 +0100

    feat: Extract and map form field values to datas object
    - Added loop to process fields object, extracting field names and values from shapes
    - Enables data collection for form submission or further processing when button is clicked
    - Improves form interaction by preparing structured data output

--- resources/js/function/entity/click_button_form.blade.php
+++ resources/js/function/entity/click_button_form.blade.php
@@ -5,8 +5,21 @@
         console.log('click button form');
 
         let fields = __FIELDS__;
-        
+
         console.log('Fields:', fields);
+
+        let datas = {};
+        for (let key in fields) {
+            
+            let fieldName = key.substring(6);
+            let fieldUid = fields[key];
+
+            let fieldShapes = shapes[fieldUid];
+            let fieldValue = fieldShapes.text;
+            datas[fieldName] = fieldValue;
+
+        }
+        console.log('Datas:', datas);
 
     }
     window['__name__']();
