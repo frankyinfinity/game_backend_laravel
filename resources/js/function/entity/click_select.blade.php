1ac94fbb580e063407c0519c8e1450560456b797
companyinfinity <companyinfinity@gmail.com>
2026-01-21 18:27:48 +0100

    refactor: scope input_uid as window property for global access in click_select.blade.php
    - Changed local variable `input_uid` to `window.input_uid` to ensure availability across functions and scopes.
    - Removed unused `heightOption` variable and redundant `moveShapes` call in scroll handlers.
    - Updated all references to use `window.input_uid` for consistency.

--- resources/js/function/entity/click_select.blade.php
+++ resources/js/function/entity/click_select.blade.php
@@ -2,15 +2,15 @@
 
     window['__name__'] = function() {
 
-        let input_uid = object['uid'].split('_')[0];
+        window.input_uid = object['uid'].split('_')[0];
 
-        let objectBody = objects[input_uid+'_body_select'];
+        let objectBody = objects[window.input_uid+'_body_select'];
         let notActiveColor = objectBody.attributes.border_not_active_color;
         let activeColor = objectBody.attributes.border_active_color;
 
         // Deactivate all other inputs
         for (let key in objects) {
-            if (key.endsWith('_body_select') && key !== input_uid + '_body_select') {
+            if (key.endsWith('_body_select') && key !== window.input_uid + '_body_select') {
                 let otherUid = key.split('_')[0];
                 let otherObjectBody = objects[key];
                 otherObjectBody.attributes.active = false;
@@ -27,75 +27,57 @@
         objectBody.attributes.active = !objectBody.attributes.active;
         let active = objectBody.attributes.active;
 
-        let shapeBorder = shapes[input_uid+'_border_select'];
+        let shapeBorder = shapes[window.input_uid+'_border_select'];
         shapeBorder.tint = active ? activeColor : notActiveColor;
 
-        let shapeValueText = shapes[input_uid + '_box_icon_text'];
+        let shapeValueText = shapes[window.input_uid + '_box_icon_text'];
         shapeValueText.text = active ? 'V' : 'A';
 
-        let shapePanel = shapes[input_uid + '_panel_select'];
+        let shapePanel = shapes[window.input_uid + '_panel_select'];
         shapePanel.renderable = active;
 
-        let objectPanel = objects[input_uid + '_panel_select'];
+        let objectPanel = objects[window.input_uid + '_panel_select'];
         objectPanel.children.forEach(function(childUid) {
             let shapeChild = shapes[childUid];
             shapeChild.zIndex = active ? 9999 : 0;
             shapeChild.renderable = active;
         });
 
-        shapes[input_uid + '_scroll_up'].renderable = active;
-        shapes[input_uid + '_scroll_up_text'].renderable = active;
-        shapes[input_uid + '_scroll_down'].renderable = active;
-        shapes[input_uid + '_scroll_down_text'].renderable = active;
+        shapes[window.input_uid + '_scroll_up'].renderable = active;
+        shapes[window.input_uid + '_scroll_up_text'].renderable = active;
+        shapes[window.input_uid + '_scroll_down'].renderable = active;
+        shapes[window.input_uid + '_scroll_down_text'].renderable = active;
 
-        updateVisibility(input_uid, objectBody.attributes.currentStart || 0, objectBody.attributes.optionShowDisplay, objectBody.attributes.optionIds, objectBody.attributes.totalOptions);
+        updateVisibility(window.input_uid, objectBody.attributes.currentStart || 0, objectBody.attributes.optionShowDisplay, objectBody.attributes.optionIds, objectBody.attributes.totalOptions);
 
     }
     window['__name__']();
 
-    window['scroll_up_' + input_uid] = function() {
-        let objectBody = objects[input_uid + '_body_select'];
+    window['scroll_up_' + window.input_uid] = function() {
+        let objectBody = objects[window.input_uid + '_body_select'];
         let currentStart = objectBody.attributes.currentStart || 0;
         let optionShowDisplay = objectBody.attributes.optionShowDisplay;
         let totalOptions = objectBody.attributes.totalOptions;
-        let heightOption = objectBody.attributes.heightOption;
         let optionIds = objectBody.attributes.optionIds;
         if (currentStart > 0) {
             currentStart--;
             objectBody.attributes.currentStart = currentStart;
-            moveShapes(uid, heightOption, optionIds, totalOptions);
-            updateVisibility(uid, currentStart, optionShowDisplay, optionIds, totalOptions);
+            updateVisibility(window.input_uid, currentStart, optionShowDisplay, optionIds, totalOptions);
         }
     };
 
-    window['scroll_down_' + input_uid] = function() {
-        let objectBody = objects[input_uid + '_body_select'];
+    window['scroll_down_' + window.input_uid] = function() {
+        let objectBody = objects[window.input_uid + '_body_select'];
         let currentStart = objectBody.attributes.currentStart || 0;
         let optionShowDisplay = objectBody.attributes.optionShowDisplay;
         let totalOptions = objectBody.attributes.totalOptions;
-        let heightOption = objectBody.attributes.heightOption;
         let optionIds = objectBody.attributes.optionIds;
         if (currentStart + optionShowDisplay < totalOptions) {
             currentStart++;
             objectBody.attributes.currentStart = currentStart;
-            moveShapes(uid, -heightOption, optionIds, totalOptions);
-            updateVisibility(uid, currentStart, optionShowDisplay, optionIds, totalOptions);
+            updateVisibility(window.input_uid, currentStart, optionShowDisplay, optionIds, totalOptions);
         }
     };
-
-    function moveShapes(uid, deltaY, optionIds, totalOptions) {
-        for (let idx = 0; idx < totalOptions; idx++) {
-            let id = optionIds[idx];
-            let rect = shapes[uid + '_option_rect_' + id];
-            let text = shapes[uid + '_option_text_' + id];
-            let border = shapes[uid + '_option_border_' + id];
-            rect.y += deltaY;
-            text.y += deltaY;
-            if (border && border.points) {
-                border.points.forEach(p => p.y += deltaY);
-            }
-        }
-    }
 
     function updateVisibility(uid, currentStart, optionShowDisplay, optionIds, totalOptions) {
         for (let idx = 0; idx < totalOptions; idx++) {
