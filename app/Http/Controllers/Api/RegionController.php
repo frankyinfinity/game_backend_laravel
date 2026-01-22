346c3340d732305786e2652991580d6eca499163
companyinfinity <companyinfinity@gmail.com>
2026-01-22 17:06:51 +0100

    call regions api in on_change_planet

--- app/Http/Controllers/Api/RegionController.php
+++ app/Http/Controllers/Api/RegionController.php
@@ -5,13 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Region;
+use Illuminate\Support\Facades\Log;
 
 class RegionController extends Controller
 {
 
     public function get($planet_id){
-        $regions = Region::query()->where('planet_id', $planet_id)->orderBy('name')->whereNotNull('filename')->get();
-        return response()->json(['success' => true, 'regions' => $regions]);
+
+        $regions = Region::query()
+            ->where('planet_id', $planet_id)
+            ->orderBy('name')
+            ->whereNotNull('filename')
+            ->get();
+
+        Log::info($regions);
+        
+        return response()->json(['success' => true]);
+
     }
     
 }
--- resources/js/function/entity/on_change_planet.blade.php
+++ resources/js/function/entity/on_change_planet.blade.php
@@ -1,6 +1,11 @@
 <script>
     window['__name__'] = function() {
         console.log('Planet changed to: ' + selectedText + ' (ID: ' + selectedId + ')');
+        
+        if (selectedId) {
+            fetch('/api/regions/' + selectedId)
+                .catch(error => console.error('Error fetching regions:', error));
+        }
     }
     window['__name__']();
 </script>
