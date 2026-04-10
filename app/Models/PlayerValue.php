82a94633a7ee7849fd17a84764343a07de0a8f65
companyinfinity2024 <companyinfinity2024@gmail.com>
2026-04-10 10:38:15 +0200

    add KEY_CHIMICAL_ELEMENT_CONSUME_PER_TICK  in PlayerValue

--- app/Models/PlayerValue.php
+++ app/Models/PlayerValue.php
@@ -19,6 +19,7 @@ class PlayerValue extends Model
     public const KEY_DIVISION_COST = 'division_cost';
     public const KEY_LIFEPOINT_GENERATE_NEW_ENTITY = 'lifepoint_generate_new_entity';
     public const KEY_CHIMICAL_ELEMENT = 'chimical_element';
+    public const KEY_CHIMICAL_ELEMENT_CONSUME_PER_TICK = 'chimical_element_consume_per_tick';
 
     public const BOOLEAN_KEYS = [
         self::KEY_MOVEMENT,
@@ -31,6 +32,7 @@ class PlayerValue extends Model
     public const INTEGER_KEYS = [
         self::KEY_DIVISION_COST => 50,
         self::KEY_LIFEPOINT_GENERATE_NEW_ENTITY => 40,
+        self::KEY_CHIMICAL_ELEMENT_CONSUME_PER_TICK => 4,
     ];
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
