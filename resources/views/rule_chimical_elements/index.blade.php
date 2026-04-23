3a24d85793e23ccb598a5c9cb703071f6976e757
companyinfinity2024 <companyinfinity2024@gmail.com>
2026-04-23 17:17:37 +0200

    delete RuleChimicalElement create view

--- resources/views/rule_chimical_elements/index.blade.php
+++ resources/views/rule_chimical_elements/index.blade.php
@@ -29,19 +29,9 @@
                         </button>
                     </div>
                     <div class="col-3">
-                        <div class="btn-group w-100" role="group">
-                            <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-plus"></i> Nuovo
-                            </button>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#createChimicalRuleModal">
-                                    <i class="fa fa-atom"></i> Elemento Chimico
-                                </a>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#createComplexChimicalRuleModal">
-                                    <i class="fa fa-cubes"></i> Elemento Complesso
-                                </a>
-                            </div>
-                        </div>
+                        <button type="button" class="btn btn-primary btn-block btn-sm" data-toggle="modal" data-target="#createRuleModal">
+                            <i class="fa fa-plus"></i> Nuovo
+                        </button>
                     </div>
                 </div>
             </div>
@@ -75,108 +65,105 @@
     </div>
 </div>
 
-<div class="modal fade" id="createChimicalRuleModal" tabindex="-1" role="dialog" aria-labelledby="createChimicalRuleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+{{-- Modal Unica di Creazione --}}
+<div class="modal fade" id="createRuleModal" tabindex="-1" role="dialog" aria-labelledby="createRuleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="createChimicalRuleModalLabel">Nuova Regola - Elemento Chimico</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <form action="{{ route('rule-chimical-elements.store') }}" method="POST">
-                @csrf
-                 <div class="modal-body">
-                      <div class="form-group">
-                          <label for="type_modal">Tipo Regola <span class="text-danger">*</span></label>
-                          <select class="form-control" id="type_modal" name="type" required>
-                              <option value="entity">Entità</option>
-                              <option value="element">Elemento</option>
-                          </select>
-                      </div>
-                      <div class="form-group">
-                          <label for="name_modal">Nome Regola <span class="text-danger">*</span></label>
-                          <input type="text" class="form-control" id="name_modal" name="name" required>
-                      </div>
-                      <div class="form-group">
-                          <label for="chimical_element_id_modal">Elemento Chimico <span class="text-danger">*</span></label>
-                          <select class="form-control" id="chimical_element_id_modal" name="chimical_element_id" required>
-                              <option value="">Seleziona Elemento Chimico</option>
-                              @foreach($chimicalElements as $ce)
-                                  <option value="{{ $ce->id }}">{{ $ce->name }} ({{ $ce->symbol }})</option>
-                              @endforeach
-                          </select>
-                      </div>
-                      <div class="form-group">
-                          <label for="min">Min <span class="text-danger">*</span></label>
-                          <input type="number" class="form-control" id="min" name="min" value="0" required>
-                      </div>
-                      <div class="form-group">
-                          <label for="max">Max <span class="text-danger">*</span></label>
-                          <input type="number" class="form-control" id="max" name="max" value="0" required>
-                      </div>
-                      <div class="form-group">
-                          <label for="default_value">Valore Default</label>
-                          <input type="number" class="form-control" id="default_value" name="default_value" value="0">
-                      </div>
-                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
-                    <button type="submit" class="btn btn-success">Salva</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="createComplexChimicalRuleModal" tabindex="-1" role="dialog" aria-labelledby="createComplexChimicalRuleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="createComplexChimicalRuleModalLabel">Nuova Regola - Elemento Chimico Complesso</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title" id="createRuleModalLabel"><i class="fas fa-plus-circle mr-2"></i> Nuova Regola Elemento Chimico</h5>
+                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <form action="{{ route('rule-chimical-elements.store') }}" method="POST">
                 @csrf
                 <div class="modal-body">
-                     <div class="form-group">
-                         <label for="type_modal_complex">Tipo Regola <span class="text-danger">*</span></label>
-                         <select class="form-control" id="type_modal_complex" name="type" required>
-                             <option value="entity">Entità</option>
-                             <option value="element">Elemento</option>
-                         </select>
-                     </div>
-                     <div class="form-group">
-                         <label for="name_modal_complex">Nome Regola <span class="text-danger">*</span></label>
-                         <input type="text" class="form-control" id="name_modal_complex" name="name" required>
-                     </div>
-                     <div class="form-group">
-                         <label for="complex_chimical_element_id_modal">Elemento Chimico Complesso <span class="text-danger">*</span></label>
-                         <select class="form-control" id="complex_chimical_element_id_modal" name="complex_chimical_element_id" required>
-                             <option value="">Seleziona Elemento Chimico Complesso</option>
-                             @foreach($complexChimicalElements as $cce)
-                                 <option value="{{ $cce->id }}">{{ $cce->name }}</option>
-                             @endforeach
-                         </select>
-                     </div>
-                     <div class="form-group">
-                         <label for="min">Min <span class="text-danger">*</span></label>
-                         <input type="number" class="form-control" id="min" name="min" value="0" required>
-                     </div>
-                     <div class="form-group">
-                         <label for="max">Max <span class="text-danger">*</span></label>
-                         <input type="number" class="form-control" id="max" name="max" value="0" required>
-                     </div>
-                     <div class="form-group">
-                         <label for="default_value_modal_complex">Valore Default</label>
-                         <input type="number" class="form-control" id="default_value_modal_complex" name="default_value" value="0">
-                     </div>
+                    <div class="row">
+                        <div class="col-md-6 border-right">
+                            <h6 class="text-bold mb-3"><i class="fas fa-info-circle mr-1"></i> Configurazione Base</h6>
+                            <div class="form-group">
+                                <label for="modal_element_type">Tipo Elemento</label>
+                                <select class="form-control" id="modal_element_type" name="element_type" onchange="toggleModalElementSelects()">
+                                    <option value="simple">Elemento Chimico Semplice</option>
+                                    <option value="complex">Elemento Chimico Complesso</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="modal_name">Nome Regola <span class="text-danger">*</span></label>
+                                <input type="text" class="form-control" id="modal_name" name="name" required placeholder="Es: Regola Ossigeno">
+                            </div>
+                            <div class="form-group" id="modal_chimical_element_group">
+                                <label for="modal_chimical_element_id">Elemento Chimico</label>
+                                <select class="form-control" id="modal_chimical_element_id" name="chimical_element_id">
+                                    <option value="">Seleziona Elemento Chimico</option>
+                                    @foreach($chimicalElements as $ce)
+                                        <option value="{{ $ce->id }}">{{ $ce->name }} ({{ $ce->symbol }})</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group" id="modal_complex_chimical_element_group" style="display: none;">
+                                <label for="modal_complex_chimical_element_id">Elemento Chimico Complesso</label>
+                                <select class="form-control" id="modal_complex_chimical_element_id" name="complex_chimical_element_id">
+                                    <option value="">Seleziona Elemento Chimico Complesso</option>
+                                    @foreach($complexChimicalElements as $cce)
+                                        <option value="{{ $cce->id }}">{{ $cce->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="modal_type">Tipo Regola</label>
+                                <select class="form-control" id="modal_type" name="type">
+                                    <option value="entity">Entità</option>
+                                    <option value="element" selected>Elemento</option>
+                                </select>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="modal_min">Min</label>
+                                        <input type="number" class="form-control" id="modal_min" name="min" value="0" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="modal_max">Max</label>
+                                        <input type="number" class="form-control" id="modal_max" name="max" value="100" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="modal_default">Default</label>
+                                        <input type="number" class="form-control" id="modal_default" name="default_value" value="0">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <h6 class="text-bold mb-3"><i class="fas fa-chart-line mr-1"></i> Degradazione</h6>
+                            <div class="form-group">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="modal_degradable" name="degradable" value="1">
+                                    <label class="custom-control-label" for="modal_degradable">Abilita Degradazione</label>
+                                </div>
+                            </div>
+                            <div id="modal_degradation_fields" style="display: none;">
+                                <div class="form-group mt-3">
+                                    <label for="modal_qty_tick">Qtà per Tick</label>
+                                    <input type="number" class="form-control" id="modal_qty_tick" name="quantity_tick_degradation" value="1" min="0">
+                                    <small class="text-muted">Quantità persa ad ogni tick di gioco.</small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="modal_pct">Degrado %</label>
+                                    <input type="number" class="form-control" id="modal_pct" name="percentage_degradation" value="100" min="0" max="100" step="0.01">
+                                    <small class="text-muted">Probabilità di accadimento (0-100%).</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="modal-footer">
+                <div class="modal-footer bg-light">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
-                    <button type="submit" class="btn btn-success">Salva</button>
+                    <button type="submit" class="btn btn-success"><i class="fas fa-save mr-1"></i> Crea Regola</button>
                 </div>
             </form>
         </div>
@@ -210,6 +197,29 @@
                 var url="{{ route('rule-chimical-elements.show',['_id_']) }}";
                 url = url.replace('_id_',$(this).data('id'));
                 window.location.href = url;
+            });
+
+            window.toggleModalElementSelects = function() {
+                var type = $('#modal_element_type').val();
+                if (type === 'simple') {
+                    $('#modal_chimical_element_group').show();
+                    $('#modal_complex_chimical_element_group').hide();
+                    $('#modal_chimical_element_id').attr('required', true);
+                    $('#modal_complex_chimical_element_id').attr('required', false);
+                } else {
+                    $('#modal_chimical_element_group').hide();
+                    $('#modal_complex_chimical_element_group').show();
+                    $('#modal_chimical_element_id').attr('required', false);
+                    $('#modal_complex_chimical_element_id').attr('required', true);
+                }
+            }
+
+            $('#modal_degradable').on('change', function() {
+                if ($(this).is(':checked')) {
+                    $('#modal_degradation_fields').slideDown();
+                } else {
+                    $('#modal_degradation_fields').slideUp();
+                }
             });
 
             var table = $("#table_list").DataTable({
