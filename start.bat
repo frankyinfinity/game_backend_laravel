1b60a5c47df0038d16615e5b1ed5013a191af98d
companyinfinity2024 <companyinfinity2024@gmail.com>
2026-07-07 11:19:23 +0200

    update start

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 start /b php artisan serve --host=0.0.0.0 --port=8085
 start /b php artisan reverb:start
-start /b php artisan queue:work --timeout=300
+start /b php artisan queue:listen --timeout=300
 gcloud compute ssh --zone "europe-west12-c" "instance-game" --project "game-500515" --tunnel-through-iap -- -N -R 8085:127.0.0.1:8085
